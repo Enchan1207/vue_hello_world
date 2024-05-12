74e4166799bc52a0a5767347f3077f5324627d70
Enchan <enchant0130@gmail.com>
2024-05-12 14:25:28 +0900

    [Upadate #1] use Clicker component
    作成したClickerコンポーネントをimportして並べて使用

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,11 @@
 <!-- ルートコンポーネント -->
-<script lang="ts">
-import { defineComponent } from 'vue';
-
-export default defineComponent({
-    data() {
-        return {
-            count: 0
-        };
-    }
-});
+<script setup lang="ts">
+import Clicker from './Clicker.vue';
 </script>
 
 <template>
     <h2>Button Clicker</h2>
-    <div id="clicker">
-        <button @click="count++">Click me!</button>
-        <span>{{ count }}</span>
-    </div>
+    <Clicker label="1"></Clicker>
+    <Clicker label="2"></Clicker>
+    <Clicker label="3"></Clicker>
 </template>
-
-<style scoped>
-#clicker{
-    max-width: 200px;
-    display: flex;
-    justify-content: space-between;
-}
-</style>
